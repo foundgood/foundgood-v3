0243a841a99fa2e18098025e5334981d7a3dd7c1
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-05-04 22:03:07 +0200

    Add small page transition

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 
 // Next
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 
 // Packages and polyfills
+import { Transition, animated } from 'react-spring';
 
 // Style
 import 'styles/foundgood.css';
@@ -14,6 +16,8 @@ import 'styles/foundgood.css';
 import LayoutWrapper from 'components/_layout/layoutWrapper';
 
 function MyApp({ Component, pageProps }) {
+    const router = useRouter();
+
     return (
         <>
             <Head>
@@ -24,9 +28,26 @@ function MyApp({ Component, pageProps }) {
                     content="width=device-width, initial-scale=1"
                 />
             </Head>
-            <LayoutWrapper {...{ pageLayout: Component.layout, pageProps }}>
-                <Component {...{ pageProps }} />
-            </LayoutWrapper>
+            <Transition
+                items={{
+                    id: router.route,
+                    Component,
+                    pageProps,
+                }}
+                keys={item => item.id}
+                from={{ opacity: 0 }}
+                initial={{ opacity: 0 }}
+                enter={{ opacity: 1 }}
+                leave={{ opacity: 0, position: 'absolute' }}>
+                {(styles, { pageProps, Component }) => (
+                    <animated.div style={{ ...styles, width: '100%' }}>
+                        <LayoutWrapper
+                            {...{ pageLayout: Component.layout, pageProps }}>
+                            <Component {...{ pageProps }} />
+                        </LayoutWrapper>
+                    </animated.div>
+                )}
+            </Transition>
         </>
     );
 }
