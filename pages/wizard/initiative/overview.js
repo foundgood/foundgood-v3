9ba925669c5cbd15e4e5c4ad5170493e0ca253af
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-05-04 21:06:55 +0200

    Add overview

--- pages/wizard/initiative/overview.js
+++ pages/wizard/initiative/overview.js
@@ -2,7 +2,11 @@
 import React from 'react';
 
 // Packages
+<<<<<<< HEAD
 import { useForm, useFormState } from 'react-hook-form';
+=======
+import { useForm } from 'react-hook-form';
+>>>>>>> b830305 (Add overview)
 import t from 'prop-types';
 
 // Utilities
@@ -10,6 +14,16 @@ import { useAuth, useMetadata } from 'utilities/hooks';
 
 // Components
 import TitlePreamble from 'components/_wizard/titlePreamble';
+<<<<<<< HEAD
+=======
+import {
+    InputWrapper,
+    Select,
+    Text,
+    LongText,
+    SelectList,
+} from 'components/_inputs';
+>>>>>>> b830305 (Add overview)
 
 const OverviewComponent = ({ pageProps }) => {
     // Hook: Verify logged in
@@ -17,14 +31,72 @@ const OverviewComponent = ({ pageProps }) => {
     verifyLoggedIn();
 
     // Hook: Metadata
+<<<<<<< HEAD
     const { labelTodo } = useMetadata();
+=======
+    const { labelTodo, log, valueSet } = useMetadata();
+
+    log();
+
+    // Hook: useForm setup
+    const { handleSubmit, control } = useForm();
+>>>>>>> b830305 (Add overview)
 
     return (
         <>
             <TitlePreamble
                 title={labelTodo('Overview')}
+<<<<<<< HEAD
                 preamble={labelTodo('')}
             />
+=======
+                preamble={labelTodo(
+                    'Preamble of what you need to do, should be clear and easy to understand'
+                )}
+            />
+            <InputWrapper>
+                <Select
+                    name="overview_responsible_organisation"
+                    label={labelTodo('Responsible organisation')}
+                    placeholder={labelTodo('Grantee name')}
+                    options={[
+                        { label: 'Test value 1', value: 'value1' },
+                        { label: 'Test value 2', value: 'value2' },
+                        { label: 'Test value 3', value: 'value3' },
+                    ]}
+                    controller={control}
+                />
+                <Text
+                    name="overview_initiative_name"
+                    label={labelTodo('What is the name of your initiative?')}
+                    placeholder={labelTodo('Title of initiative')}
+                    maxLength={30}
+                    controller={control}
+                />
+                <LongText
+                    name="overview_initiative_about"
+                    label={labelTodo('What are your initiative about')}
+                    placeholder={labelTodo(
+                        "Brief description of initiative that details why it's important"
+                    )}
+                    maxLength={400}
+                    controller={control}
+                />
+                <SelectList
+                    name="overview_initiative_location"
+                    showText
+                    label={labelTodo('Where is it located?')}
+                    listMaxLength={3}
+                    options={valueSet('account.Location__c').map(item => ({
+                        label: item.label,
+                        value: item.fullName,
+                    }))}
+                    selectLabel={labelTodo('Country')}
+                    textLabel={labelTodo('Region')}
+                    controller={control}
+                />
+            </InputWrapper>
+>>>>>>> b830305 (Add overview)
         </>
     );
 };
