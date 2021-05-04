37a0e440676ae2dd8d85dbceb4c25bbf1dcd1fdf
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-05-04 21:06:38 +0200

    Adjust inputs to have more consistent prop api

--- components/_inputs/text/text.js
+++ components/_inputs/text/text.js
@@ -4,49 +4,64 @@ import React, { useState } from 'react';
 // Packages
 import cc from 'classcat';
 import t from 'prop-types';
+import { Controller } from 'react-hook-form';
 
-const TextComponent = React.forwardRef(
-    (
-        { label, subLabel, defaultValue, error, maxLength, onChange, ...rest },
-        ref
-    ) => {
-        // Local state for handling char count
-        const [value, setValue] = useState(defaultValue || '');
+const TextComponent = ({
+    name,
+    label,
+    subLabel,
+    defaultValue,
+    maxLength,
+    controller,
+    ...rest
+}) => {
+    // Local state for handling char count
+    const [value, setValue] = useState(defaultValue || '');
 
-        return (
-            <label className="flex flex-col">
-                {label && <span className="input-label">{label}</span>}
-                {subLabel && (
-                    <span className="mt-8 input-sublabel">{subLabel}</span>
+    return (
+        <label className="flex flex-col">
+            {label && <span className="input-label">{label}</span>}
+            {subLabel && (
+                <span className="mt-8 input-sublabel">{subLabel}</span>
+            )}
+            <Controller
+                control={controller}
+                defaultValue={defaultValue}
+                name={name}
+                rules={{ maxLength }}
+                render={({
+                    field: { onChange, onBlur, value, ref },
+                    fieldState: { error },
+                }) => (
+                    <input
+                        ref={ref}
+                        type="text"
+                        defaultValue={defaultValue}
+                        maxLength={maxLength ? maxLength : 'none'}
+                        onChange={event => {
+                            // Local value state
+                            setValue(event.target.value);
+                            onChange(event);
+                        }}
+                        className={cc([
+                            'input-defaults',
+                            {
+                                'ring-2 ring-coral-300 bg-coral-10 text-coral-300': error,
+                                'mt-16': label,
+                            },
+                        ])}
+                        {...rest}
+                    />
                 )}
-                <input
-                    ref={ref}
-                    type="text"
-                    defaultValue={defaultValue}
-                    maxLength={maxLength ? maxLength : 'none'}
-                    onChange={event => {
-                        // Local value state
-                        setValue(event.target.value);
-                        onChange(event);
-                    }}
-                    className={cc([
-                        'input-defaults',
-                        {
-                            'ring-2 ring-coral-300 bg-coral-10 text-coral-300': error,
-                            'mt-16': label,
-                        },
-                    ])}
-                    {...rest}
-                />
-                {maxLength > 0 && (
-                    <div className="mt-4 -mb-16 text-right input-utility-text">
-                        {value.length} / {maxLength.toString()}
-                    </div>
-                )}
-            </label>
-        );
-    }
-);
+            />
+            {maxLength > 0 && (
+                <div className="mt-4 -mb-16 text-right input-utility-text">
+                    {value.length} / {maxLength.toString()}
+                </div>
+            )}
+        </label>
+    );
+};
 
 TextComponent.propTypes = {
     name: t.string,
