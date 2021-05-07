163c7c0137021ede91ba9b4cc3a60de3b776df27
robinisaksson <robin.isaksson@protonmail.com>
2021-05-07 10:01:06 +0200

    Fix test styling

--- components/_report/numberCard/numberCard.js
+++ components/_report/numberCard/numberCard.js
@@ -3,16 +3,12 @@ import React from 'react';
 
 // Packages
 import t from 'prop-types';
-import Image from 'next/image';
-
-// Components
-import SectionWrapper from 'components/_report/sectionWrapper';
 
 const NumberCardComponent = ({ number, headline, description }) => {
     return (
         <div className="p-16 border-4 rounded-4 border-blue-10">
             <div className="text-blue-100 t-h1">{number}</div>
-            <div className="text-blue-60 t-sh7">{headline}</div>
+            <div className="text-blue-60 t-footnote-bold">{headline}</div>
             <div className="text-blue-60 t-footnote">{description}</div>
         </div>
     );
