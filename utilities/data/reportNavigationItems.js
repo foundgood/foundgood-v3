35f952f8f1213e117779d3a72f2884f44977a6aa
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-05-18 21:00:32 +0200

    Getting readier for labels in wizards

--- utilities/data/reportNavigationItems.js
+++ utilities/data/reportNavigationItems.js
@@ -11,18 +11,46 @@ const reportItems = () => [
         items: [
             {
                 title: 'Report details',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/report-details',
             },
             {
                 title: 'Funders',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/funders',
             },
             {
                 title: 'Overview',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/overview',
             },
             {
                 title: 'Report Summary',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/report-summary',
             },
         ],
@@ -33,34 +61,90 @@ const reportItems = () => [
         items: [
             {
                 title: 'Applicants',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/applicants',
             },
             {
                 title: 'Collaborators',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/collaborators',
             },
             {
                 title: 'Employees funded',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/employees-funded',
             },
             {
                 title: 'Goals',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/goals',
             },
             {
                 title: 'Activities',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/activities',
             },
             {
                 title: 'Indicators',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/indicators',
             },
             {
                 title: 'Progress so far',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/progress-so-far',
             },
             {
                 title: 'Sharing results',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/sharing-results',
             },
         ],
@@ -71,13 +155,32 @@ const reportItems = () => [
         items: [
             {
                 title: 'Influence on policy',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/influence-on-policy',
             },
             {
                 title: 'Evaluations',
+                labels: {
+                    form: {
+                        title: '',
+                        preamble: '',
+                    },
+                    help: { title: '', why: '', what: '', guide: '' },
+                },
                 url: '/wizard/evaluations',
             },
         ],
+    },
+    {
+        title: 'Done',
+        visible: false,
+        url: '/',
     },
 ];
 
