a901e94279b7e522131daee9197075bbd7d6ee35
robinisaksson <robin.isaksson@protonmail.com>
2021-05-20 13:26:09 +0200

    Get report summary texts

--- utilities/api/salesForce/queries.js
+++ utilities/api/salesForce/queries.js
@@ -45,7 +45,7 @@ const queries = {
         },
     },
     initiativeReport: {
-        _query: `SELECT Id, CreatedById, LastModifiedDate, Name, Summary_Of_Activities__c, Summary_Of_Challenges_And_Learnings__c, Executive_Summary__c, Funder_Report__c, Report_Type__c, Report_Period_Start_Date__c, Report_Period_End_Date__c, Status__c, toLabel(Status__c) Translated_Status__c, Due_Date__c, UserRecordAccess.HasReadAccess, UserRecordAccess.HasEditAccess, UserRecordAccess.HasDeleteAccess FROM Initiative_Report__c`,
+        _query: `SELECT Id, CreatedById, LastModifiedDate, Name, Project_Purpose__c, Progress_Towards_Grant_Area_Themes__c, Important_Results__c, Post_Project_Activities__c, Summary_Of_Activities__c, Summary_Of_Challenges_And_Learnings__c, Executive_Summary__c, Funder_Report__c, Report_Type__c, Report_Period_Start_Date__c, Report_Period_End_Date__c, Status__c, toLabel(Status__c) Translated_Status__c, Due_Date__c, UserRecordAccess.HasReadAccess, UserRecordAccess.HasEditAccess, UserRecordAccess.HasDeleteAccess FROM Initiative_Report__c`,
         get(id) {
             return `${this._query} WHERE Id = '${id}'`;
         },
