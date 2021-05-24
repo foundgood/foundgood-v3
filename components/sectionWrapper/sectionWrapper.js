0671cf3462ea73825ebdf20dd8dc05b4acd16fe7
robinisaksson <robin.isaksson@protonmail.com>
2021-05-24 11:51:22 +0200

    Add section IDs & update CONSTANTS

--- components/sectionWrapper/sectionWrapper.js
+++ components/sectionWrapper/sectionWrapper.js
@@ -8,18 +8,21 @@ import t from 'prop-types';
 const SectionWrapperComponent = ({
     children,
     className,
-    useBackground = false,
     paddingY = true,
+    id,
 }) => {
     const elementClassNames = cc([
         { 'px-16 md:px-32': !paddingY },
         {
-            // 'px-16 pt-16 pb-12 md:px-32 md:pt-32 md:pb-24 xl:px-64 xl:pt-64 xl:pb-52': paddingY,
             'px-16 pt-16 pb-12 md:px-32 md:pt-32 md:pb-24': paddingY,
         },
         className,
     ]);
-    return <div className={elementClassNames}>{children}</div>;
+    return (
+        <div className={elementClassNames} id={id}>
+            {children}
+        </div>
+    );
 };
 
 SectionWrapperComponent.propTypes = {};
