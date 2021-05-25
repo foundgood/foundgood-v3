2c59495541f25be8e2fc65c7443da9509afad550
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2021-05-25 13:50:20 +0200

    ADD BUTTON

--- components/updateButton/updateButton.js
+++ components/updateButton/updateButton.js
@@ -14,6 +14,7 @@ import {
 import { useInitiativeDataStore } from 'utilities/store';
 
 // Components
+import Button from 'components/button';
 
 const UpdateButtonComponent = ({ mode, baseUrl }) => {
     // Hook: Context
