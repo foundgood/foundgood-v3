93e519a28663f535919b9d0b7e33b90b85de734a
robinisaksson <robin.isaksson@protonmail.com>
2021-05-25 16:53:30 +0200

    Alternative layout - if no description

--- components/_initiative/reportDetailCard/reportDetailCard.js
+++ components/_initiative/reportDetailCard/reportDetailCard.js
@@ -2,6 +2,7 @@
 import React from 'react';
 
 // Packages
+import cc from 'classcat';
 import t from 'prop-types';
 import Image from 'next/image';
 
@@ -10,8 +11,15 @@ import { isJson } from 'utilities';
 
 const ReportDetailCardComponent = ({ headline, image, description, items }) => {
     return (
-        <div className="flex justify-between">
-            <div className="mr-24">
+        <div
+            className={cc([
+                'flex',
+                {
+                    'justify-between flex-col md:flex-row': description,
+                    'flex-col': !description,
+                },
+            ])}>
+            <div className={cc([{ 'mr-24': description }])}>
                 <div className="flex">
                     {image && (
                         <div className="h-24 mr-8 overflow-hidden rounded-4">
@@ -20,7 +28,6 @@ const ReportDetailCardComponent = ({ headline, image, description, items }) => {
                     )}
                     <div className="t-h6">{headline}</div>
                 </div>
-
                 {description &&
                     isJson(description) &&
                     JSON.parse(description).map(item => (
@@ -32,13 +39,19 @@ const ReportDetailCardComponent = ({ headline, image, description, items }) => {
                     <p className="mt-16">{description}</p>
                 )}
             </div>
-
             {items && (
-                <div className="flex flex-col flex-none w-1/3">
+                <div
+                    className={cc([
+                        'flex',
+                        { 'flex-col flex-none md:w-1/3': description },
+                    ])}>
                     {items.map((item, index) => (
                         <div
                             key={`i-${index}`}
-                            className="p-16 mt-8 border-4 border-blue-10 rounded-4">
+                            className={cc([
+                                'p-16 mt-8 border-4 border-blue-10 rounded-4',
+                                { 'mr-8': !description },
+                            ])}>
                             <div className="t-sh7">{item.label}</div>
                             <div className="t-caption-bold">{item.text}</div>
                         </div>
@@ -47,6 +60,78 @@ const ReportDetailCardComponent = ({ headline, image, description, items }) => {
             )}
         </div>
     );
+    // return (
+    //     <>
+    //         {/*
+    //         Two different layouts depending on if description exists
+    //         */}
+    //         {description && (
+    //             <div className="flex justify-between">
+    //                 <div className="mr-24">
+    //                     <div className="flex">
+    //                         {image && (
+    //                             <div className="h-24 mr-8 overflow-hidden rounded-4">
+    //                                 <Image src={image} width="24" height="24" />
+    //                             </div>
+    //                         )}
+    //                         <div className="t-h6">{headline}</div>
+    //                     </div>
+    //                     {description &&
+    //                         isJson(description) &&
+    //                         JSON.parse(description).map(item => (
+    //                             <p key={item.id} className="mt-16">
+    //                                 {item.text}
+    //                             </p>
+    //                         ))}
+    //                     {description && !isJson(description) && (
+    //                         <p className="mt-16">{description}</p>
+    //                     )}
+    //                 </div>
+    //                 {items && (
+    //                     <div className="flex flex-col flex-none w-1/3">
+    //                         {items.map((item, index) => (
+    //                             <div
+    //                                 key={`i-${index}`}
+    //                                 className="p-16 mt-8 border-4 border-blue-10 rounded-4">
+    //                                 <div className="t-sh7">{item.label}</div>
+    //                                 <div className="t-caption-bold">
+    //                                     {item.text}
+    //                                 </div>
+    //                             </div>
+    //                         ))}
+    //                     </div>
+    //                 )}
+    //             </div>
+    //         )}
+    //         {/* No description - Left align items under the headline */}
+    //         {!description && (
+    //             <div className="flex flex-col">
+    //                 <div className="flex">
+    //                     {image && (
+    //                         <div className="h-24 mr-8 overflow-hidden rounded-4">
+    //                             <Image src={image} width="24" height="24" />
+    //                         </div>
+    //                     )}
+    //                     <div className="t-h6">{headline}</div>
+    //                 </div>
+    //                 {items && (
+    //                     <div className="flex">
+    //                         {items.map((item, index) => (
+    //                             <div
+    //                                 key={`i-${index}`}
+    //                                 className="p-16 mt-8 mr-8 border-4 border-blue-10 rounded-4">
+    //                                 <div className="t-sh7">{item.label}</div>
+    //                                 <div className="mt-2 t-caption-bold">
+    //                                     {item.text}
+    //                                 </div>
+    //                             </div>
+    //                         ))}
+    //                     </div>
+    //                 )}
+    //             </div>
+    //         )}
+    //     </>
+    // );
 };
 
 ReportDetailCardComponent.propTypes = {
@@ -68,3 +153,63 @@ ReportDetailCardComponent.propTypes = {
 ReportDetailCardComponent.defaultProps = {};
 
 export default ReportDetailCardComponent;
+
+// All in one html
+// return (
+//     <div
+//         className={cc([
+//             'flex ',
+//             {
+//                 'bg-teal-20 justify-between': description,
+//                 'bg-amber-20 flex-col': !description,
+//             },
+//         ])}>
+//         <div
+//             className={cc([
+//                 {
+//                     'mr-24 bg-teal-60': description,
+//                     'bg-amber-60': !description,
+//                 },
+//             ])}>
+//             <div className="flex">
+//                 {image && (
+//                     <div className="h-24 mr-8 overflow-hidden rounded-4">
+//                         <Image src={image} width="24" height="24" />
+//                     </div>
+//                 )}
+//                 <div className="t-h6">{headline}</div>
+//             </div>
+
+//             {description &&
+//                 isJson(description) &&
+//                 JSON.parse(description).map(item => (
+//                     <p key={item.id} className="mt-16">
+//                         {item.text}
+//                     </p>
+//                 ))}
+//             {description && !isJson(description) && (
+//                 <p className="mt-16">{description}</p>
+//             )}
+//         </div>
+
+//         {items && (
+//             <div
+//                 className={cc([
+//                     'flex flex-none w-1/3',
+//                     {
+//                         'flex-col bg-teal-100': description,
+//                         'bg-amber-100': !description,
+//                     },
+//                 ])}>
+//                 {items.map((item, index) => (
+//                     <div
+//                         key={`i-${index}`}
+//                         className="p-16 mt-8 border-4 border-blue-10 rounded-4">
+//                         <div className="t-sh7">{item.label}</div>
+//                         <div className="t-caption-bold">{item.text}</div>
+//                     </div>
+//                 ))}
+//             </div>
+//         )}
+//     </div>
+// );
