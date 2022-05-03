d95cb61ebd466e7e5be4cb03e583150d732279a8
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2022-05-03 19:31:58 +0200

    Remove unused salesForce

--- utilities/api/salesForce/index.js
+++ utilities/api/salesForce/index.js
@@ -2,6 +2,5 @@ import { queries } from './queries';
 import * as user from './user';
 import * as fetchers from './fetchers';
 import * as custom from './custom';
-import * as crud from './crud';
 
-export default { user, fetchers, queries, custom, crud };
+export default { user, fetchers, queries, custom };
