479454a266088ff88cf9dbf1c1fc1fe4cc7db871
Anders Hallundbæk <anders.hallundbaek@gmail.com>
2022-07-01 12:44:39 +0200

    Label

--- components/_wizard/_cards/_cardContents/activityTeamEducation/activityTeamEducation.js
+++ components/_wizard/_cards/_cardContents/activityTeamEducation/activityTeamEducation.js
@@ -64,7 +64,7 @@ const ActivityTeamEducationComponent = ({ item }) => {
                 <div>
                     <h5 className="mt-16 t-caption-bold text-teal-60">
                         {object.label(
-                            'Initiative_Activity_Regulation__c.Employee__c'
+                            'Initiative_Activity_Regulation__c.Employee__c__TeamEducation'
                         )}
                     </h5>
                     <div className="flex flex-col mt-2 space-y-4">
